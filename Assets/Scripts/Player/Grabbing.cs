7544187f8f025b748f31a55ca8910741e80a1a89
survivorr <kangur20040@wp.pl>
2021-11-20 16:04:44 +0100

    basics
    +calculate and carry on velocity of grabbed object after dropping
    +prevent grabbed object from going through colliders

--- Assets/Scripts/Player/Grabbing.cs
+++ Assets/Scripts/Player/Grabbing.cs
@@ -7,7 +7,8 @@ public class Grabbing : MonoBehaviour {
 	public LayerMask layerMask;
 	public KeyCode grabKey;
 	private Transform _playerTransform;
-	
+	private Vector3 _cubeVelocity;
+	private Vector3 _cubeLatePosition;
 	[Header("Values")]
 	[Range(1, 16)]
 	public float maxDistance = 6f;
@@ -24,10 +25,12 @@ public class Grabbing : MonoBehaviour {
 
 	private GameObject fixedGrabbingPointGameObject;
 	private Vector3 localGrabbingPoint;
+	private Vector3 grabbingPointStartPosition;
+	
 
 	private void Start() {
 		_playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-
+		grabbingPointStartPosition = grabbingPoint.transform.localPosition;
 		fixedGrabbingPointGameObject = Instantiate(
 			new GameObject("FixedGrabbingPoint"),
 			grabbingPoint.position,
@@ -38,11 +41,19 @@ public class Grabbing : MonoBehaviour {
 
 	private void Update() {
 		RaycastHit hit;
+		RaycastHit depthScan;
 		Transform origin = Camera.main.transform;
 
+		if(Physics.Raycast(origin.position, origin.forward, out depthScan, maxDistance, layerMask)) {
+			Debug.Log(depthScan.transform.gameObject);
+			grabbingPoint.transform.position = Vector3.Lerp(grabbingPoint.transform.position, depthScan.point - Camera.main.transform.forward * 0.8f, 12f*Time.deltaTime);
+		}else{
+			grabbingPoint.transform.localPosition = Vector3.Lerp(grabbingPoint.transform.localPosition, grabbingPointStartPosition, 4f*Time.deltaTime);
+		}
 		if (Input.GetKeyDown(grabKey)) {
 			// Dropping held object
 			if (_grabbedObject) {
+				_grabbedObject.GetComponent<Rigidbody>().velocity = _cubeVelocity/Time.deltaTime;
 				_grabbedObject.GetComponent<Rigidbody>().useGravity = true;
 				IgnoreGrabbedObjectCollision(false);
 				_grabbedObject = null;
@@ -51,7 +62,7 @@ public class Grabbing : MonoBehaviour {
 			}
 
 			// Grabbing object
-			if (Physics.Raycast(origin.position, origin.forward, out hit, maxDistance, layerMask)) {
+			if (Physics.Raycast(origin.position, origin.forward, out hit, maxDistance, ~layerMask)) {
 				if (hit.rigidbody) {
 					_grabbedObject = hit.collider.gameObject;
 					_grabbedRigidbody = _grabbedObject.GetComponent<Rigidbody>();
@@ -60,7 +71,6 @@ public class Grabbing : MonoBehaviour {
 				}
 			}
 		}
-
 		if (_grabbedObject) {
 			localGrabbingPoint = _playerTransform.InverseTransformPoint(grabbingPoint.position);
 
@@ -84,8 +94,11 @@ public class Grabbing : MonoBehaviour {
 			_grabbedRigidbody.velocity = Vector3.zero;
 			_grabbedRigidbody.angularVelocity = Vector3.zero;
 		}
-
+		if(_grabbedObject)_cubeVelocity = _grabbedObject.transform.position - _cubeLatePosition;
 		Debug.DrawRay(origin.position, origin.forward * maxDistance, Color.green);
+	}
+	void LateUpdate(){
+		if(_grabbedObject)_cubeLatePosition = _grabbedObject.transform.position;	
 	}
 
 	private void IgnoreGrabbedObjectCollision(bool ignore) {
